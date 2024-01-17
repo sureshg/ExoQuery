473cb3ef989081e9669085aecf3a183761fb385a
Alexander Ioffe <deusaquilus@gmail.com>
2024-01-17 02:18:21 -0500

    Move join transform to upper level

---
+++ exoquery-plugin-kotlin/src/main/kotlin/io/exoquery/plugin/transform/TransformJoinOn.kt
@@ -0,0 +1,58 @@
+package io.exoquery.plugin.transform
+
+import io.decomat.Is
+import io.decomat.case
+import io.decomat.on
+import io.exoquery.Lambda1Expression
+import io.exoquery.plugin.logging.CompileLogger
+import io.exoquery.plugin.logging.Messages
+import io.exoquery.plugin.safeName
+import io.exoquery.plugin.trees.ExtractorsDomain
+import io.exoquery.plugin.trees.Parser
+import io.exoquery.plugin.trees.ParserContext
+import io.exoquery.plugin.trees.TypeParser
+import io.exoquery.xr.XR
+import org.jetbrains.kotlin.ir.expressions.IrCall
+import org.jetbrains.kotlin.ir.expressions.IrExpression
+
+class TransformJoinOn(override val ctx: TransformerOrigin): Transformer() {
+  context(BuilderContext, CompileLogger)
+  override fun matchesBase(expression: IrCall): Boolean =
+    ExtractorsDomain.Call.`join-on(expr)`.matchesMethod(expression)
+
+  context(ParserContext, BuilderContext, CompileLogger)
+  override fun transformBase(expression: IrCall, superTransformer: io.exoquery.plugin.CaptureTransformer): IrExpression {
+    val (caller, funExpression, params, blockBody) =
+      on(expression).match(
+        case(ExtractorsDomain.Call.`join-on(expr)`[Is()]).then { queryCallData -> queryCallData }
+      ) ?: parseFail("Illegal block on function:\n${Messages.PrintingMessage(expression)}")
+
+    //error("-------- Function:\n${funExpression.function.dumpSimple()}\n------------Params: ${funExpression.function.extensionReceiverParameter?.symbol?.safeName}")
+
+     //There actually IS a reciver to this function and it should be named $this$on
+    val reciverParam = funExpression.function.extensionReceiverParameter ?: illegalStruct("Extension Reciever for on-clause was null")
+    val reciverSymbol = reciverParam.symbol.safeName
+    val paramIdent = run {
+      val tpe = TypeParser.parse(reciverParam.type)
+      XR.Ident(reciverSymbol, tpe)
+    }
+
+    // parse the `on` clause of the join.on(...)
+    val (onLambdaBody, bindsAccum) =
+      with(makeParserContext().copy(internalVars + ScopeSymbols(listOf(reciverParam.symbol)))) {
+        Parser.parseFunctionBlockBody(blockBody)
+      }
+
+    val onLambda = Lambda1Expression(XR.Function1(paramIdent, onLambdaBody))
+    val onLambdaExpr = lifter.liftExpression(onLambda)
+    // To transform the TableQuery etc... in the join(<Heree>).on clause before the `on`
+    // No scope symbols into caller since it comes Before the on-clause i.e. before any symbols could be created
+    val newCaller = caller.transform(superTransformer, internalVars)
+
+    warn("------------ Binds Accum -----------\n" + bindsAccum.show())
+
+    val bindsList = bindsAccum.toDynamicBindsExpr()
+
+    return newCaller.callMethod("onExpr").invoke(onLambdaExpr, bindsList)
+  }
+}
--- exoquery-plugin-kotlin/src/main/kotlin/io/exoquery/plugin/transform/TransformQueryFlatMap.kt
+++ exoquery-plugin-kotlin/src/main/kotlin/io/exoquery/plugin/transform/TransformQueryFlatMap.kt
@@ -3,63 +3,14 @@ package io.exoquery.plugin.transform
 import io.decomat.Is
 import io.decomat.case
 import io.decomat.on
-import io.exoquery.Lambda1Expression
 import io.exoquery.SqlVariable
 import io.exoquery.plugin.logging.CompileLogger
 import io.exoquery.plugin.logging.Messages
-import io.exoquery.plugin.printing.dumpSimple
-import io.exoquery.plugin.safeName
 import io.exoquery.plugin.trees.*
 import io.exoquery.xr.XR
 import org.jetbrains.kotlin.ir.declarations.IrValueParameter
 import org.jetbrains.kotlin.ir.expressions.IrCall
 import org.jetbrains.kotlin.ir.expressions.IrExpression
-import io.exoquery.plugin.trees.ExtractorsDomain.Call.`join-on(expr)`
-import io.exoquery.xr.XRType
-import org.jetbrains.kotlin.ir.backend.js.utils.typeArguments
-import org.jetbrains.kotlin.ir.util.dumpKotlinLike
-
-class TransformJoinOn(override val ctx: TransformerOrigin): Transformer() {
-  context(BuilderContext, CompileLogger)
-  override fun matchesBase(expression: IrCall): Boolean =
-    `join-on(expr)`.matchesMethod(expression)
-
-  context(ParserContext, BuilderContext, CompileLogger)
-  override fun transformBase(expression: IrCall, superTransformer: io.exoquery.plugin.CaptureTransformer): IrExpression {
-    val (caller, funExpression, params, blockBody) =
-      on(expression).match(
-        case(`join-on(expr)`[Is()]).then { queryCallData -> queryCallData }
-      ) ?: parseFail("Illegal block on function:\n${Messages.PrintingMessage(expression)}")
-
-    //error("-------- Function:\n${funExpression.function.dumpSimple()}\n------------Params: ${funExpression.function.extensionReceiverParameter?.symbol?.safeName}")
-
-     //There actually IS a reciver to this function and it should be named $this$on
-    val reciverParam = funExpression.function.extensionReceiverParameter ?: illegalStruct("Extension Reciever for on-clause was null")
-    val reciverSymbol = reciverParam.symbol.safeName
-    val paramIdent = run {
-      val tpe = TypeParser.parse(reciverParam.type)
-      XR.Ident(reciverSymbol, tpe)
-    }
-
-    // parse the `on` clause of the join.on(...)
-    val (onLambdaBody, bindsAccum) =
-      with(makeParserContext().copy(internalVars + ScopeSymbols(listOf(reciverParam.symbol)))) {
-        Parser.parseFunctionBlockBody(blockBody)
-      }
-
-    val onLambda = Lambda1Expression(XR.Function1(paramIdent, onLambdaBody))
-    val onLambdaExpr = lifter.liftExpression(onLambda)
-    // To transform the TableQuery etc... in the join(<Heree>).on clause before the `on`
-    // No scope symbols into caller since it comes Before the on-clause i.e. before any symbols could be created
-    val newCaller = caller.transform(superTransformer, internalVars)
-
-    warn("------------ Binds Accum -----------\n" + bindsAccum.show())
-
-    val bindsList = bindsAccum.toDynamicBindsExpr()
-
-    return newCaller.callMethod("onExpr").invoke(onLambdaExpr, bindsList)
-  }
-}
 
 class TransformQueryFlatMap(override val ctx: TransformerOrigin, val replacementMethod: String): Transformer() {
   private val matcher = ExtractorsDomain.Call.QueryFlatMap
