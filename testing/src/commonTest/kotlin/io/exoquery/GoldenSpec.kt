3fcc5792ea3129d94385b269681e21538b26f44c
Alexander Ioffe <deusaquilus@gmail.com>
2025-02-12 01:43:42 -0500

    Plans for dynamic golden spec

--- testing/src/commonTest/kotlin/io/exoquery/GoldenSpec.kt
+++ testing/src/commonTest/kotlin/io/exoquery/GoldenSpec.kt
@@ -1,10 +1,42 @@
 package io.exoquery
 
+import io.exoquery.xr.XR
 import io.kotest.core.spec.DslDrivenSpec
-import io.kotest.core.spec.style.FreeSpec
 import io.kotest.core.spec.style.scopes.FreeSpecRootScope
-import io.kotest.matchers.equals.shouldBeEqual
 import kotlin.test.assertEquals
+
+sealed interface Mode {
+  object ExoGoldenTest : Mode
+  object ExoGoldenOverride: Mode
+}
+
+abstract class GoldenSpecDynamic(val goldenQueries: GoldenQueryFile, val mode: Mode, body: io.exoquery.GoldenSpecDynamic.() -> Unit): DslDrivenSpec(), FreeSpecRootScope {
+
+  // TODO the show() function should use the mirror idiom once that is complete
+  fun XR.shouldBeGolden(label: String) = this.show().shouldBeGolden(label)
+  fun SqlCompiledQuery<*>.shouldBeGolden() = this.value.shouldBeGolden(this.label ?: error("""The following query did not have a label: "$value""""))
+
+  fun String.shouldBeGolden(label: String) =
+    when (mode) {
+      is Mode.ExoGoldenTest ->
+        goldenQueries.queries[label]?.let {
+          assertEquals(
+            it.trimIndent().trim(), this.trimIndent().trim(),
+            "Golden query for label: \"$label\" did not match"
+          )
+        } ?: error("""No golden query found for label: "$label"""")
+
+      // TODO collect into a data structure & print the contents of the GoldenQueryFile when the test is complete
+      //      use the writer in the ExoQuery compiler plugin here to do the same (might want to move that writer to the Runtime module)
+      is Mode.ExoGoldenOverride ->
+        TODO()
+    }
+
+  init {
+    body()
+  }
+}
+
 
 // This mimics FreeSpec but adds a shouldBeGolden function that compares the compiled query to a golden query
 // whenever I try to extend FreeSpec I get the following error:
