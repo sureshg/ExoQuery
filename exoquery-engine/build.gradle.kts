37251cee54134f1f63e1c148dab902dcb78ae71f
Alexander Ioffe <deusaquilus@gmail.com>
2025-05-06 17:32:16 -0400

    Remove JB annotations from engine (for now)

--- exoquery-engine/build.gradle.kts
+++ exoquery-engine/build.gradle.kts
@@ -46,7 +46,6 @@ kotlin {
         // This is a java-only library, I have no idea who it can even be here.
         // maybe if I actually attempt to use it in KMP mode in a non-java target it will actually fail
         api("com.github.vertical-blank:sql-formatter:2.0.4")
-        implementation("com.sschr15.annotations:jb-annotations-kmp:24.1.0+apple")
       }
     }
 
