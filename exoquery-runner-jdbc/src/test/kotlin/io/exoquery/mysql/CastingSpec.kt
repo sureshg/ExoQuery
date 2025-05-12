6c281c82416fe721ad0b69a89009fe2812919ee9
Alexander Ioffe <deusaquilus@gmail.com>
2025-05-12 02:40:04 -0400

    Add casting tests

---
+++ exoquery-runner-jdbc/src/test/kotlin/io/exoquery/mysql/CastingSpec.kt
@@ -0,0 +1,37 @@
+package io.exoquery.mysql
+
+import io.exoquery.SqlQuery
+import io.exoquery.TestDatabases
+import io.exoquery.capture
+import io.exoquery.runOn
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+
+class CastingSpec : FreeSpec({
+  val ctx = TestDatabases.mysql
+
+  "String to Long" {
+    val q = capture { free("SELECT '1' AS value").asPure<SqlQuery<String>>().map { it.toLong() } }
+    q.buildFor.MySql().runOn(ctx).first() shouldBe 1
+  }
+  "String to Int" {
+    val q = capture { free("SELECT '1' AS value").asPure<SqlQuery<String>>().map { it.toInt() } }
+    q.buildFor.MySql().runOn(ctx).first() shouldBe 1
+  }
+  "String to Short" {
+    val q = capture { free("SELECT '1' AS value").asPure<SqlQuery<String>>().map { it.toShort() } }
+    q.buildFor.MySql().runOn(ctx).first() shouldBe 1
+  }
+  "String to Double" {
+    val q = capture { free("SELECT '1.2' AS value").asPure<SqlQuery<String>>().map { it.toDouble() } }
+    q.buildFor.MySql().runOn(ctx).first() shouldBe (1.2).toDouble()
+  }
+  "String to Float" {
+    val q = capture { free("SELECT '1.2' AS value").asPure<SqlQuery<String>>().map { it.toFloat() } }
+    q.buildFor.MySql().runOn(ctx).first() shouldBe (1.2).toFloat()
+  }
+  "String to Boolean" {
+    val q = capture { free("SELECT 'true' AS value").asPure<SqlQuery<String>>().map { it.toBoolean() } }
+    q.buildFor.MySql().runOn(ctx).first() shouldBe true
+  }
+})
